8a64ccbbde1d71ba1def8cc2168b7fd7bba07175
anja-rasoloarivalona <rasoloanja@gmail.com>
2019-09-16 22:11:56 -0400

    Built Navtop component and added style

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Navtop from './components/navigation/navtop/Navtop';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Hi
+      <div className="app">
+        <Navtop />
       </div>
     );
   }
