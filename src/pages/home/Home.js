50ab214e5d6fa02042a693483c1d968f1ed29b0f
anja-rasoloarivalona <rasoloanja@gmail.com>
2019-09-17 13:16:19 -0400

    Init home router section and added style

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import './Home.css';
+import IconSvg from '../../utilities/svg/svg';
 
 const home = () => {
     return (
         <div className="home">
-            home
-            <p>
-                fkwnlfewknlwqfnqwfqw
-            </p>
+
+            <section className="home__intro">
+
+            </section>
+
+            <section className="home__router">
+
+                <div className="home__router__nav">
+                    <h1><span>WELCOME TO&nbsp;</span><span>WOTO MOTORS</span></h1>
+                    <nav className="home__router__nav__list">
+                        <li className="home__router__nav__list__item">Inventory</li>
+                        <li className="home__router__nav__list__item">Search</li>
+                        <li className="home__router__nav__list__item">Contact</li>
+                    </nav>
+                    <div className="home__router__nav__count">
+                        <IconSvg icon="car"/>
+                        <div>Available 99 cars</div>
+                    </div>
+                </div>
+
+            </section>
+      
         </div>
     )
 }
