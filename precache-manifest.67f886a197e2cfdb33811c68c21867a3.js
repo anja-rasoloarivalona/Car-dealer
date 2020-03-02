09d913dadf1ef695f263237d4b34c9a536683d96
anja <rasoloanja@gmail.com>
2020-03-02 10:58:32 -0500

    Updates

---
+++ precache-manifest.67f886a197e2cfdb33811c68c21867a3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2478536fe135ebc9328cf3141911aa6f",
+    "url": "/Car-dealer/index.html"
+  },
+  {
+    "revision": "0c0b47c5af772a1bdf25",
+    "url": "/Car-dealer/static/css/2.6d7d54d2.chunk.css"
+  },
+  {
+    "revision": "48aa6688bb01f96b5a27",
+    "url": "/Car-dealer/static/css/main.4fba9b63.chunk.css"
+  },
+  {
+    "revision": "0c0b47c5af772a1bdf25",
+    "url": "/Car-dealer/static/js/2.c0e623c7.chunk.js"
+  },
+  {
+    "revision": "270bd3baadcd3e629547e0c9e6559506",
+    "url": "/Car-dealer/static/js/2.c0e623c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48aa6688bb01f96b5a27",
+    "url": "/Car-dealer/static/js/main.ad2424ec.chunk.js"
+  },
+  {
+    "revision": "6a82ad25ab37999a340e",
+    "url": "/Car-dealer/static/js/runtime-main.52fae80a.js"
+  },
+  {
+    "revision": "4b55d60c83fc9fb0d6b962dd2785cd6b",
+    "url": "/Car-dealer/static/media/desert-suv.4b55d60c.jpg"
+  },
+  {
+    "revision": "b5e7acadf2f28537c01f00da3611a794",
+    "url": "/Car-dealer/static/media/eventually.b5e7acad.mp3"
+  },
+  {
+    "revision": "40e159c37ef85be7e6d5811dcd9b5ef4",
+    "url": "/Car-dealer/static/media/help.40e159c3.jpg"
+  },
+  {
+    "revision": "be8a817b89560dbb1240be6439a594ce",
+    "url": "/Car-dealer/static/media/sprite.be8a817b.svg"
+  }
+]);
