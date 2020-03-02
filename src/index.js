e36c439ebab3882b6d4341ffe921b430677b6b6c
anja <rasoloanja@gmail.com>
2020-03-02 10:57:06 -0500

    Prepared deployment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
@@ -36,9 +36,9 @@ const store = createStore(
 ReactDOM.render(
 
     <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter >
             <App />
-        </BrowserRouter>
+        </HashRouter >
     </Provider>
     
 , 
