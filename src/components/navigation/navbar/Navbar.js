2167e34a49a28afe6d3cf078c0ee23a6b1a71d69
anja <rasoloanja@gmail.com>
2020-03-10 11:45:54 -0400

    Added a component behind the navigation bar to avoid jumping up when the navigation bar is fixed after a scroll.

--- src/components/navigation/navbar/Navbar.js
+++ src/components/navigation/navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import './Navbar.css';
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
@@ -10,43 +10,38 @@ const Navbar= props => {
     let userName = props.userName;
         let isAuth = props.isAuth
         return (
-            <nav className={`navbarContainer 
-                            ${props.scrolled ? 'fixed': ''}
-                            `}>
-            <ul className="navbar__list">
-                    <NavLink exact to="/" className="navbar__list__item">
-                        <FormattedMessage id="home" defaultMessage="home"/>
-                    </NavLink>
-                    <Link to="../inventory" className={`navbar__list__item ${props.location.pathname.includes('inventory') ?  'active' : ''}`}>
-                        <FormattedMessage id="inventory" defaultMessage="inventory"/>
-                    </Link>
-                    <Link to="../services" className={`navbar__list__item ${props.location.pathname.includes('services') ?  'active' : ''}`}>
-                        <FormattedMessage id="services" defaultMessage="services"/>
-                    </Link>
-            </ul>
-
-            <div className="navbar__cta">
-
-                
-                {!isAuth && (
-                        <Link className="navbar__cta__login" to="../auth">
-                            <IconSvg icon="user"/>
-                            <span><FormattedMessage id="login" defaultMessage="login"/></span>
+            <Fragment>
+                <div className="navbarFiller"></div>
+                <nav className={`navbarContainer ${props.scrolled ? 'fixed': ''}`}>
+                    <ul className="navbar__list">
+                        <NavLink exact to="/" className="navbar__list__item">
+                            <FormattedMessage id="home" defaultMessage="home"/>
+                        </NavLink>
+                        <Link to="../inventory" className={`navbar__list__item ${props.location.pathname.includes('inventory') ?  'active' : ''}`}>
+                            <FormattedMessage id="inventory" defaultMessage="inventory"/>
                         </Link>
-                )}
-
-                {isAuth && (
-                    <div className="navbar__cta__avatar"
-                         onClick={() => props.history.push('/my-account/messages')}>
-                       <span>{userName.split(' ')[0].slice(0, 1)}{userName.split(' ')[1].slice(0, 1)}</span> 
-                    </div>
-
-                )}
-            </div>
-        </nav>
+                        <Link to="../services" className={`navbar__list__item ${props.location.pathname.includes('services') ?  'active' : ''}`}>
+                            <FormattedMessage id="services" defaultMessage="services"/>
+                        </Link>
+                    </ul>
+                    <div className="navbar__cta">
+                    {!isAuth && (
+                            <Link className="navbar__cta__login" to="../auth">
+                                <IconSvg icon="user"/>
+                                <span><FormattedMessage id="login" defaultMessage="login"/></span>
+                            </Link>
+                    )}
+                    {isAuth && (
+                        <div className="navbar__cta__avatar"
+                            onClick={() => props.history.push('/my-account/messages')}>
+                        <span>{userName.split(' ')[0].slice(0, 1)}{userName.split(' ')[1].slice(0, 1)}</span> 
+                        </div>
+                    )}
+                </div>
+            </nav>
+        </Fragment>
         )
 }
-
 const mapStateToProps = state => {
     return {
         isAuth: state.auth.auth,
